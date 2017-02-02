b1a9fead067b6111c12a6aa379d6d46506c0cd09
phillyai <sonagi.philly@gmail.com>
2017-02-02 15:20:52 +0900

    Add MainForm

--- hitomi/MainForm.Designer.cs
+++ hitomi/MainForm.Designer.cs
@@ -28,60 +28,316 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabControl1.SuspendLayout();
+            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.미리보기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button5 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
-            // tabControl1
+            // listView1
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabControl1.Location = new System.Drawing.Point(0, 0);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(463, 260);
-            this.tabControl1.TabIndex = 0;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(10, 99);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(320, 335);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
             // 
-            // tabPage1
+            // button1
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(455, 234);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "URL";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.button1.Location = new System.Drawing.Point(525, 440);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "다운로드";
+            this.button1.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // comboBox2
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(455, 234);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "크롤러";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "태그",
+            "시리즈",
+            "작가"});
+            this.comboBox2.Location = new System.Drawing.Point(10, 12);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(76, 20);
+            this.comboBox2.TabIndex = 13;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(92, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(161, 21);
+            this.textBox1.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(8, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 24);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "언어 :";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "all",
+            "BahasaIndonesia",
+            "català",
+            "Čeština",
+            "Dansk",
+            "Deutsch",
+            "eesti",
+            "English",
+            "Español",
+            "Esperanto",
+            "Français",
+            "Italiano",
+            "magyar",
+            "Nederlands",
+            "norsk",
+            "polski",
+            "Português",
+            "română",
+            "shqip",
+            "Slovenčina",
+            "Suomi",
+            "Svenska",
+            "Tagalog",
+            "tiếngviệt",
+            "Türkçe",
+            "Ελληνικά",
+            "Русский",
+            "Українська",
+            "עברית",
+            "العربية",
+            "ไทย",
+            "한국어",
+            "中文",
+            "日本語"});
+            this.comboBox1.Location = new System.Drawing.Point(92, 39);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(161, 20);
+            this.comboBox1.TabIndex = 15;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(259, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(71, 50);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "게☆또";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "이름";
+            this.columnHeader1.Width = 186;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "번호";
+            this.columnHeader2.Width = 110;
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView2.FullRowSelect = true;
+            this.listView2.GridLines = true;
+            this.listView2.Location = new System.Drawing.Point(336, 99);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(320, 335);
+            this.listView2.TabIndex = 17;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "이름";
+            this.columnHeader3.Width = 186;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "번호";
+            this.columnHeader4.Width = 110;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(336, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 24);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "다운로드 경로 :";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(439, 65);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(153, 21);
+            this.textBox2.TabIndex = 19;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(598, 65);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(58, 23);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "지정";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(383, 440);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(136, 23);
+            this.button4.TabIndex = 21;
+            this.button4.Text = "다운로드 폴더 열기";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(74, 445);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(167, 12);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "더블 클릭으로 추가/삭제 가능";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.미리보기ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(123, 26);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // 미리보기ToolStripMenuItem
+            // 
+            this.미리보기ToolStripMenuItem.Name = "미리보기ToolStripMenuItem";
+            this.미리보기ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.미리보기ToolStripMenuItem.Text = "미리보기";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(8, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 24);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "가져올 개수 :";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(92, 65);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(161, 21);
+            this.numericUpDown1.TabIndex = 25;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(259, 65);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(71, 21);
+            this.button5.TabIndex = 26;
+            this.button5.Text = "더더";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(463, 260);
-            this.Controls.Add(this.tabControl1);
+            this.ClientSize = new System.Drawing.Size(668, 470);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
             this.Name = "MainForm";
-            this.Text = "MainForm";
-            this.tabControl1.ResumeLayout(false);
+            this.Text = "다운로더";
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 미리보기ToolStripMenuItem;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button5;
     }
 }
--- hitomi/MainForm.cs
+++ hitomi/MainForm.cs
@@ -11,9 +11,106 @@ namespace hitomi
 {
     public partial class MainForm : Form
     {
+        private readonly string[] _langs = new string[]
+        {
+            "all",
+"indonesian",
+"catalan",
+"czech",
+"danish",
+"german",
+"estonian",
+"english",
+"spanish",
+"esperanto",
+"french",
+"italian",
+"hungarian",
+"dutch",
+"norwegian",
+"polish",
+"portuguese",
+"romanian",
+"albanian",
+"slovak",
+"finnish",
+"swedish",
+"tagalog",
+"vietnamese",
+"turkish",
+"greek",
+"russian",
+"ukrainian",
+"hebrew",
+"arabic",
+"thai",
+"korean",
+"chinese",
+"japanese"
+        };
         public MainForm()
         {
             InitializeComponent();
+
+            comboBox2.SelectedIndex = 0; //태그
+            comboBox1.SelectedIndex = 31; //한국어
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog f = new FolderBrowserDialog();
+            if(f.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = f.SelectedPath;
+            }
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+            if(listView1.SelectedItems.Count == 1)
+            {
+                ToolStripMenuItem item = new ToolStripMenuItem("미리보기", ((Manga)listView1.SelectedItems[0].Tag).GetImages().First());
+                contextMenuStrip1.Items.Clear();
+                contextMenuStrip1.Items.Add(item);
+                contextMenuStrip1.Items[0].DisplayStyle = ToolStripItemDisplayStyle.Image;
+            }
+        }
+
+        private IEnumerable<int> list;
+        private int index = 0;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string language = _langs[comboBox1.SelectedIndex];
+            if (comboBox2.Text == "태그")
+                list = Hitomi.CrawlFromTag(textBox1.Text, language);
+            else if (comboBox2.Text == "시리즈")
+                list = Hitomi.CrawlFromSeries(textBox1.Text, language);
+            index = 0;
+
+            foreach (int i in list.Skip(index).Take((int)numericUpDown1.Value))
+            {
+                Manga m = new Manga(i);
+                ListViewItem item = new ListViewItem(m.Name);
+                item.SubItems.Add(m.Number);
+                item.Tag = m;
+                listView1.Items.Add(item);
+            }
+
+            index += (int)numericUpDown1.Value;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            foreach (int i in list.Skip(index).Take((int)numericUpDown1.Value))
+            {
+                Manga m = new Manga(i);
+                ListViewItem item = new ListViewItem(m.Name);
+                item.SubItems.Add(m.Number);
+                item.Tag = m;
+                listView1.Items.Add(item);
+            }
+
+            index += (int)numericUpDown1.Value;
         }
     }
 }
